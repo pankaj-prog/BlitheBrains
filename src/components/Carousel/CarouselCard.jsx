a640c54c338ce9af52a9d594744b47b0cef4caee
pankaj-prog <wadhwanip38@gmail.com>
2022-04-07 00:43:17 +0530

    refactor: carousel card

--- src/components/Carousel/CarouselCard.jsx
+++ src/components/Carousel/CarouselCard.jsx
@@ -4,12 +4,15 @@ const CarouselCard = ({ video }) => {
   return (
     <article className="carousel-card">
       <img src={video.thumbnail} alt={video.title} />
-      <button
-        className="btn btn-solid-secondary btn-rc"
-        onClick={() => console.log("clicked")}
-      >
-        Watch Now
-      </button>
+      <div className="content">
+        <h2 className="title text-center">{video.title} </h2>
+        <button
+          className="btn btn-solid-secondary btn-rc"
+          onClick={() => console.log("clicked")}
+        >
+          Watch Now
+        </button>
+      </div>
     </article>
   );
 };
